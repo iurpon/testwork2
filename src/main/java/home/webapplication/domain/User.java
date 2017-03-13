341611c2aff92e480fa7e70e073d388bfffdfb96
iurpon <iurpon@mail.ru>
2017-03-13 09:58:24 +0300

    Create User.java

---
+++ src/main/java/home/webapplication/domain/User.java
@@ -0,0 +1,80 @@
+package home.webapplication.domain;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "test")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "isAdmin")
+    private boolean isAdmin;
+
+    @Column(name = "createdDate")
+    private Date timeStamp;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.isAdmin = admin;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", admin=" + isAdmin +
+                ", timeStamp=" + timeStamp +
+                '}';
+    }
+}
+
+
